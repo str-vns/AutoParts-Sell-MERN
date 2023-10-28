20cf9bedbf6cc48f8da66973585e01b0ea424467
str-vns <102575234+str-vns@users.noreply.github.com>
2023-10-28 22:52:37 +0800

    Backend ups

---
+++ backend/utility/apiFeatures.js
@@ -0,0 +1,48 @@
+class APIFeatures
+{
+   constructor(query, queryStringy)
+   {
+    this.query = query;
+    this.queryStringy = queryStringy;
+   }
+
+   search()
+   {
+    console.log(this.queryStringy)
+    const keyword = this.queryStringy.keyword ? {
+    name:
+    {
+        $reyex: req.queryStringy.keyword,
+        $options: 'i'
+    }
+   } : {}
+   console.log(this.queryStringy,keyword, this.query);
+   this.query = this.query.find({...keyword});
+   return this;
+   }
+
+   filter()
+   {
+    const queryCopy = { ...this.queryStringy};
+    const removeFields = ['keyword', 'limit', 'page']
+    removeFields.forEach(el => delete queryCopy[el]);
+
+    let queryStringy = JSON.stringify(queryCopy);
+    console.log(queryStringy);
+    queryStringy = queryStringy.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
+    console.log(JSON.parse(queryStringy));
+    this.query = this.query.find(JSON.parse(queryStringy));
+
+    return this;
+   }
+
+   pagination(PerPage)
+   {
+    const currentPage = Number(this.queryStringy.page) || 1;
+    const skip = PerPage * (currentPage - 1);
+    this.query = this.query.limit(PerPage).skip(skip);
+    return thisl
+   }
+}
+
+module.exports = APIFeatures;
