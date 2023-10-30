2fb430dd95040d56792501e85a3ab2b4ebde48be
str-vns <102575234+str-vns@users.noreply.github.com>
2023-10-30 21:33:19 +0800

    node_mod

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
-const cookieParser = require('cookie-parser')
+const cookie = require('cookie-parser')
 const cors = require('cors')
 const products = require('./routes/product')
 const auths = require('./routes/auth')
+const orders =  require('./routes/order')
+const shipping = require('./routes/shipping')
 
 app.use(cors());
-app.use(express());
-app.use(express.urlencoded({limit:"1000mb", extended: true }));
-app.use(cookieParser());
+app.use(express.json({limit:'100mb'}));
+app.use(express.urlencoded({limit:"100mb", extended: true }));
+app.use(cookie());
 
 app.use('/api/v1', products);
 app.use('/api/v1', auths);
+app.use('/api/v1', orders);
+app.use('/api/v1', shipping);
 module.exports = app
