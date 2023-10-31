34e3cfda7942266d6f01cb3d15e9a9c160ca6f23
str-vns <102575234+str-vns@users.noreply.github.com>
2023-10-31 11:32:49 +0800

    FrontEnd Latest addition

---
+++ frontend/src/Components/Home.jsx
@@ -0,0 +1,13 @@
+import { Container } from 'postcss'
+import React from 'react'
+import Loader from './Layout/Loader'
+
+function Home() {
+  return (
+    <div className='bg-white'>
+   <Loader /> 
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/src/Components/Layout/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+   <div className="h-screen flex items-center justify-center ">
+    <img src="./images/turbo.gif" className='h-32 w-50 '/>
+
+   </div>
+  )
+}
+
+export default Loader
