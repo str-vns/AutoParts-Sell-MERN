166a015f52fc8e52ae97e6f1a976df3a933165a5
str-vns <102575234+str-vns@users.noreply.github.com>
2023-11-04 23:23:24 +0800

    Young

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,12 +2,13 @@ const express = require('express');
 const passport = require('passport')
 const router = express.Router();
 const upload = require('../utility/multer');
-const { registerUser, UserLogin, UserLogout, allUsers, getUserDetails, getUserProfile, updatePassword, resetPassword, forgotPassword, updateProfile } = require('../controllers/authContoller');
+const { registerUser, UserLogin, UserLogout, allUsers, getUserDetails, getUserProfile, updatePassword, resetPassword, forgotPassword, updateProfile, googleLogin } = require('../controllers/authContoller');
 
 //user
 
 
 router.post('/register', upload.single("avatar"), registerUser);
+router.post('/google_login', googleLogin)
 router.post('/login', UserLogin)
 router.get('/logout', UserLogout)
 router.post('/password/forgot', forgotPassword);
