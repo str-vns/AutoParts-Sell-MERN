03d03d8f7f92c28ab2718384ca8c9f953ba7d9b9
str-vns <102575234+str-vns@users.noreply.github.com>
2023-11-09 14:00:28 +0800

    user profile update langs

--- frontend/src/Components/User/UpdatePassword.jsx
+++ frontend/src/Components/User/UpdatePassword.jsx
@@ -61,33 +61,33 @@ const UpdatePassword = () => {
         <Fragment>
             <MetaData title={'Change Password'} />
 
-            <div className="row wrapper">
-                <div className="col-10 col-lg-5">
-                    <form className="shadow-lg" onSubmit={submitHandler} >
-                        <h1 className="mt-2 mb-5">Update Password</h1>
-                        <div className="form-group">
-                            <label htmlFor="old_password_field">Old Password</label>
+            <div className="px-4 py-40 sm:px-6 lg:px-8 bg-white">
+                <div className="mx-auto max-w-lg text-center border-2 p-10 border-black">
+                    <form onSubmit={submitHandler} >
+                        <h1 className="text-2xl font-bold sm:text-3xl text-black">Update Password</h1>
+                        <div className="form-group mx-auto mb-0 mt-8 max-w-md space-y-4">
+                            <label htmlFor="old_password_field" className=' text-mg text-black pl-2'>Old Password</label>
                             <input
                                 type="password"
                                 id="old_password_field"
-                                className="form-control"
+                                className="form-control w-full rounded-lg border-2 text-black border-black p-4 text-sm shadow-sm bg-white"
                                 value={oldPassword}
                                 onChange={(e) => setOldPassword(e.target.value)}
                             />
                         </div>
 
-                        <div className="form-group">
-                            <label htmlFor="new_password_field">New Password</label>
+                        <div className="form-group mx-auto mb-0 mt-8 max-w-md space-y-4">
+                            <label htmlFor="new_password_field" className=' text-mg text-black pl-2'>New Password</label>
                             <input
                                 type="password"
                                 id="new_password_field"
-                                className="form-control"
+                                className="form-control w-full rounded-lg border-2 text-black border-black p-4 text-sm shadow-sm bg-white"
                                 value={password}
                                 onChange={(e) => setPassword(e.target.value)}
                             />
                         </div>
 
-                        <button type="submit" className="btn update-btn btn-block mt-4 mb-3" disabled={loading ? true : false} >Update Password</button>
+                        <button type="submit" className="inline-block rounded-lg bg-black px-5 py-3 mt-5 text-sm font-medium text-white hover:border-2 hover:border-black hover:bg-white hover:text-black" disabled={loading ? true : false} >Update Password</button>
                     </form>
                 </div>
             </div>
--- frontend/src/Components/User/UpdateProfile.jsx
+++ frontend/src/Components/User/UpdateProfile.jsx
@@ -111,17 +111,17 @@ const UpdateProfile = () => {
         <Fragment>
             <Metadata title={'Update Profile'} />
 
-            <div className="row wrapper">
-                <div className="col-10 col-lg-5">
-                    <form className="shadow-lg" onSubmit={submitHandler} encType='multipart/form-data'>
-                        <h1 className="mt-2 mb-5">Update Profile</h1>
+            <div className="px-4 py-40 sm:px-6 lg:px-8 bg-white">
+                <div className="mx-auto max-w-lg text-center border-2 p-10 border-black">
+                    <form  onSubmit={submitHandler} encType='multipart/form-data'>
+                        <h1 className="text-2xl font-bold sm:text-3xl text-black">Update Profile</h1>
 
                         <div className="form-group">
-                            <label htmlFor="email_field">Name</label>
+                            <label htmlFor="email_field" className='text-mg text-black text-left flex'>Name</label>
                             <input
                                 type="name"
                                 id="name_field"
-                                className="form-control"
+                                className="form-control mt-1 p-4 lg:w-full md:w-full sm:w-full rounded-md border-2 h-10 border-black bg-white text-sm text-gray-700 shadow-sm"
                                 name='name'
                                 value={name}
                                 onChange={(e) => setName(e.target.value)}
@@ -130,13 +130,13 @@ const UpdateProfile = () => {
 
 
                         <div className='form-group'>
-                            <label htmlFor='avatar_upload'>Avatar</label>
+                            <label htmlFor='avatar_upload' className=' text-mg text-black text-left flex'>Avatar</label>
                             <div className='d-flex align-items-center'>
                                 <div>
                                     <figure className='avatar mr-3 item-rtl'>
                                         <img
                                             src={avatarPreview}
-                                            className='rounded-circle'
+                                            className=' rounded-circle w-16 h-16  object-cover p-10'
                                             alt='Avatar Preview'
                                         />
                                     </figure>
@@ -145,12 +145,12 @@ const UpdateProfile = () => {
                                     <input
                                         type='file'
                                         name='avatar'
-                                        className='custom-file-input'
+                                        className='hidden '
                                         id='customFile'
                                         accept='image/*'
                                         onChange={onChange}
                                     />
-                                    <label className='custom-file-label' htmlFor='customFile'>
+                                    <label className='custom-file-label float-left px-4 py-2 border-2 border-black rounded-md cursor-pointer bg-white text-black hover:bg-black hover:text-white' htmlFor='customFile'>
                                         Choose Avatar
                                     </label>
                                 </div>
