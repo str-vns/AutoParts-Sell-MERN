9a10182908dbea982c2f76fa4aa4265b5cc42c8f
str-vns <102575234+str-vns@users.noreply.github.com>
2023-11-18 19:51:47 +0800

    routes update

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../utility/multer')
 const { getproducts, GetOneProduct, newProducts, getAdminproducts, updateProducts, deleteProducts, createReview } = require('../controllers/productController');
-const {isAuthenticatedUser} =require('../middlewares/auth');
+const {isAuthenticatedUser, authorizeRoles} =require('../middlewares/auth');
 
 //user
 router.get('/products', getproducts)
 router.get('/product/:id', GetOneProduct);
 
 //admin
-router.get('/admin/products', getAdminproducts);
-router.post('/admin/product/new',  upload.array('images', 10), newProducts);
+router.get('/admin/products', isAuthenticatedUser, authorizeRoles('admin'), getAdminproducts);
+router.post('/admin/product/new', isAuthenticatedUser, upload.array('images', 10), newProducts);
 router.route('/admin/product/:id', ).put(upload.array('images', 10), updateProducts).delete(deleteProducts);
 
 
